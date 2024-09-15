d76aa94fd6f4f5a2d452833024f4f48b64c50e24
thehasanovv <hasanov.e95@gmail.com>
2024-09-15 14:27:24 +0400

    valid added

--- src/main/java/az/atl/productms/model/request/SaveProductDto.java
+++ src/main/java/az/atl/productms/model/request/SaveProductDto.java
@@ -1,5 +1,7 @@
 package az.atl.productms.model.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +16,13 @@ import static lombok.AccessLevel.PRIVATE;
 @AllArgsConstructor
 @FieldDefaults(level = PRIVATE)
 public class SaveProductDto {
+
+    @Size(min = 3, max = 50, message = "Name Required - The name must be between 3 and 50 characters.")
     String name;
+
+    @NotNull(message = "Price Required")
     BigDecimal price;
+
+    @NotNull(message = "Count Required")
     Integer count;
 }
